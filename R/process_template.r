76dfe694cceb6384d1e9f4ac07a566e9e0865b8c
Jiří Moravec <jiri.c.moravec@gmail.com>
2019-11-27 09:11:32 +1300

    code for processing XML template

---
+++ R/process_template.r
@@ -0,0 +1,94 @@
+#' Process an XML template with TOML config.
+#'
+#' \code{process_template} will parse TOML config with three different blocks, xml containing
+#' additional xml chunks that will be processed independently, templates containing file paths
+#' relative to the parsed TOML config) to additional TOML configs and defaults contain default
+#' parameters for main template, xml chunks contained in the TOML config or any subtemplates linked
+#' in the TOML config. The xml chunks and any parameters are then substituted to the template
+#' according to {{ moustache }} tags.
+#'
+#' First the function will recursively parse any TOML configs linked in the template block and
+#' then process XML chunks by substituting the {{ moustache }} tags using the content of the
+#' default block and any parameters from the parent TOML config. After that, processed XML chunks
+#' as well as any other values in default block of TOML config or from input parameters are
+#' substituted into the XML template according to the present {{ moustache }} tags.
+#'
+#' There are two different ways how parameters and xml chunks sharing the same name are merged.
+#' Parameters will overwrite parameters with the same name in their daughter config. This means
+#' that subtemplates/subconfigs can be shared by different TOML configs and be parametrized
+#' according to their need. On the other hand, xml chunks with the same name are merged together
+#' as a list, this makes it possible for different subtemplates to define their own priors,
+#' operators or logs, which can be then put in a single point without replacing each other's
+#' structures. This makes it possible to build a BEAST2 XML.
+#'
+#' @param template an XML template with {{ moustache }} tags that will be substituted according to
+#'     the input parameters or the default parameters in the TOML config.
+#' @param config a TOML config file containing default values for {{ moustache }} tags, XML chunks
+#'     or path to additional TOML subconfigs/subtemplates.
+#' @param output processed template
+#' @param parameters these will replace the parameters with the same name in the TOML config
+#'     or any TOML subconfigs/subtemplates
+#' @export
+process_template = function(template, config, output, parameters=NULL){
+    config = parse_config(config, parameters)
+    data = merge(config$data, merge(config$defaults, parameters))
+    template = readLines(template)
+    text = whisker::whisker.render(template, data)
+    writeLines(text, output)
+    }
+
+
+#' Recursively parse TOML conig
+#̈́'
+#' First recursively parse any TOML configs linked in the template block and then process XML
+#' chunks by substituting the {{ moustache }} tags using the content of the default block and
+#' any parameters from the parent TOML config.
+#'
+#' There are two different ways how parameters and xml chunks sharing the same name are merged.
+#' Parameters will overwrite parameters with the same name in their daughter config. This means
+#' that subtemplates/subconfigs can be shared by different TOML configs and be parametrized
+#' according to their need. On the other hand, xml chunks with the same name are merged together
+#' as a list, this makes it possible for different subtemplates to define their own priors,
+#' operators or logs, which can be then put in a single point without replacing each other's
+#' structures. This makes it possible to build a BEAST2 XML. 
+#'
+#' @param file TOML config
+#' @param defaults parameters that will replace any other parameters from the default block
+#' @return list of processed XML chunks from this or any daughter config
+#'     and the default parameter block
+parse_config = function(file, defaults=NULL){
+    config = RcppTOML::parseTOML(file, escape=FALSE)
+    settmpdir(dirname(file))
+
+    defaults = merge(config$defaults, defaults)
+    data = list()
+    if(!is.null(config$xml))
+        data = add(data, process_xml_chunks(config$xml, defaults))
+    if(!is.null(config$templates))
+        data = add(data, process_subtemplates(config$templates, defaults))
+    return(list("data" = data, "defaults" = defaults))
+    }
+
+
+#' Process TOML subtemplates/subconfigs in the templates block
+#'
+#' @param subtemplates list of TOML subtemplates from the templates block
+#' @param defaults parameters
+process_subtemplates = function(subtemplates, defaults){  
+    data = list()
+    for(subtemplate in subtemplates){
+        data = add(data, parse_config(subtemplate, defaults)$data)
+        }
+    data
+    }
+
+
+#' Process list of xml chunks
+#'
+#' process xml chunks, replacing the {{ moustache }} tags with the parameters
+#' @param xml_chunks list of strings with {{ moustache }} tags
+#' @param parameters parameters to substitute {{ moustache }} tags
+#' @return list of processed xml chunks
+process_xml_chunks = function(xml_chunks, parameters){
+    lapply(xml_chunks, whisker::whisker.render, data=parameters)
+    }
---
+++ tests/testthat/test_process_template.r
@@ -0,0 +1,187 @@
+context("Processing XML template and TOML config")
+
+
+
+# Helper functions
+get_template = function(template, config, parameters=NULL){
+    temp = tempfile()
+    on.exit(unlink(temp))
+    process_template(template, config, temp, parameters)
+    readLines(temp)
+    }
+
+
+write_to_temp = function(text){
+    tempfile = tempfile()
+    writeLines(text, tempfile)
+    tempfile
+    }
+
+# Empty config -- to make creating subtemplates easier
+empty_config = list(
+    "xml" = list(),
+    "templates" = list(),
+    "defaults" = list()
+    )
+
+# A simple config
+config = list(
+    "xml" = list(
+        "chunk_one" = "This is chunk {{one}}",
+        "chunk_two" = "This is chunk {{two}}"
+        ),
+    "templates" = list(),
+    "defaults" = list(
+        "one" = 1,
+        "two" = 2
+        )
+    )
+
+processed = list(
+    "chunk_one" = "This is chunk 1",
+    "chunk_two" = "This is chunk 2"
+        )
+
+# Test process_xml_chunks ------------------------------------------------------------------------ #
+test_that("Can render XML chunks in isolation", {
+    xml_chunks = config$xml
+    parameters = config$defaults
+    expected = processed
+    expect_equal(process_xml_chunks(xml_chunks, parameters), expected)
+    })
+
+
+# Test parse_config ------------------------------------------------------------------------------ #
+test_that("Can parse simple config", {
+    config_file = write_to_temp(list2toml(config))
+    expected = list(
+        "data" = processed,
+        "defaults" = config$defaults
+        )
+    expect_equal(parse_config(config_file), expected)
+
+    unlink(config_file)
+    })
+
+
+test_that("Can parse config with a subtemplates", {
+    # set up subtemplate
+    subtemplate = empty_config
+    subtemplate$xml[["chunk_three"]] = "This is chunk {{three}}"
+    subtemplate$defaults[["three"]] = 3
+    subtemplate_file = write_to_temp(list2toml(subtemplate))
+
+    # set up config
+    config$templates[["subtemplate"]] = subtemplate_file
+    config_file = write_to_temp(list2toml(config))
+
+    # set up expected results
+    expected = list(
+        "data" = c(processed, "chunk_three" = "This is chunk 3"),
+        "defaults" = config$defaults
+        )
+
+    expect_equal(parse_config(config_file), expected)
+
+    # clean
+    unlink(subtemplate_file)
+    unlink(config_file)
+    })
+
+
+test_that("XML chunks with the same name do merge", {
+    # set up subtemplate
+    subtemplate = empty_config
+    subtemplate$xml[["chunk_two"]] = "Just another chunk in the wall!"
+    subtemplate_file = write_to_temp(list2toml(subtemplate))
+
+    # set up config
+    config$templates[["subtemplate"]] = subtemplate_file
+    config_file = write_to_temp(list2toml(config))
+
+    # set up expected results
+    expected = list(
+        "data" = processed,
+        "defaults" = config$defaults
+        )
+    expected$data[["chunk_two"]] = c(expected$data[["chunk_two"]], subtemplate$xml[["chunk_two"]])
+
+    expect_equal(parse_config(config_file), expected)
+
+    # clean
+    unlink(subtemplate_file)
+    unlink(config_file)
+    })
+
+
+test_that("Additional parameters overwrite defaults", {
+    config_file = write_to_temp(list2toml(config))
+
+    expected = list(
+        "data" = processed,
+        "defaults" = config$defaults
+        )
+    expected$data$chunk_one = "This is chunk 3"
+    expected$defaults$one = 3
+
+    expect_equal(parse_config(config_file, list("one"=3)), expected)
+
+    unlink(config_file)
+    })
+
+
+# Test process_template -------------------------------------------------------------------------- #
+test_that("Test basic template processing", {
+    processed_template = get_template("test_files/test.xml", "test_files/test.toml") 
+    premade_template = readLines("test_files/test_processed.xml")
+
+    expect_equal(processed_template, premade_template)
+    })
+
+
+test_that("processing XML chunks with {{ mustache }} tags", {
+    config_file = write_to_temp(list2toml(config))
+    template = "{{chunk_one}}\n{{chunk_two}}"
+    template_file = write_to_temp(template)
+    expected = paste("This is chunk", 1:2)
+
+    processed = get_template(template_file, config_file)
+
+    expect_equal(processed, expected)
+
+    unlink(config_file)
+    unlink(template_file)
+    })
+
+
+test_that("XML chunks with the same name are added together", {
+    # set up subtemplate
+    subtemplate = empty_config
+    subtemplate$xml[["chunk"]] = "Subtemplate chunk"
+    subtemplate_file = write_to_temp(list2toml(subtemplate))
+
+    # set up config
+    config = empty_config
+    config$xml[["chunk"]] = "Template chunk"
+    config$templates[["subtemplate"]] = subtemplate_file
+    config_file = write_to_temp(list2toml(config))
+
+    # setup template
+    template = "{{#chunk}}{{.}}\n{{/chunk}}"
+    template_file = write_to_temp(template)
+
+    expected = c("Template chunk", "Subtemplate chunk", "")
+
+    expect_equal(get_template(template_file, config_file), expected)
+
+    unlink(subtemplate_file)
+    unlink(config_file)
+    unlink(template_file)
+    })
+
+# Test:
+# processing XML chunks with mustache
+# processing XML chunks with mustache in subtemplates
+# merging XML chunks into the same list in subtemplates
+# parameters rewrite defaults
+# parameters rewrite defaults in subtemplates
