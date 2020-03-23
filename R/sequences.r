043cee4ae56348b494d23b1b58d2e3f56016609f
Jiří Moravec <jiri.c.moravec@gmail.com>
2020-03-23 18:08:34 +1300

    Functions and associated tests to process sequence alignment and generate BEAST xml data block.

---
+++ R/sequences.r
@@ -0,0 +1,209 @@
+#' Process and XML template given alignment
+#'
+#' \code{todoname} is a more user-friendly version of \code{process_template} function. Using this
+#' function, user only needs the XML template constructed by modifying BEAUTi output and
+#' an alignment file (e.g., fasta). The function parses the alignment input and tries to estimate
+#' some parameters regarding the alignment required by BEAST, such as the data type
+#' (e.g., nucleotide, standard...), size of the alignment
+#todoname = function(template, alignment, output, parameters=NULL){
+#    sequences = read_alignment(alignment)
+#    data = list(
+#        datatype = get_datatype(sequences)
+#        
+#    data = merge(alignment$data, parameters)
+#    template = readLines(template)
+#    text = whisker::whisker.render(template, data)
+#    writeLines(text, output)
+#    }
+
+
+#' Read alignment
+#'
+#' Parse sequence alignment and return a named list of sequences.
+#'
+#' The function \code{\link{read_alignment}} reads the alignment according to the format provided
+#' with the \code{format} parameter or, if not provided, tries to guess the alignment format from
+#' the file extension. Currently supported formats are the \emph{fasta} and \emph{nexus} formats.
+#' Interleaved formats are not supported.
+#'
+#'
+#' @param file with alignment
+#' @param format requested format
+#' @return named list of sequences
+#' @name readalignment
+read_alignment = function(file, format=NULL){
+    supported_formats = c("fasta", "nexus")
+
+    if(!is.null(format)){
+        if(!format %in% supported_formats) stop("Unsupported format: ", format)
+        if(format == "fasta") return(read_fasta(file))
+        if(format == "nexus") return(read_nexus(file))
+
+        } else {
+
+        ext = tolower(tools::file_ext(file))
+
+        if(ext %in% c("fasta", "fst", "fas")) return(read_fasta(file))
+        if(ext %in% c("nexus", "nex")) return(read_nexus(file))
+        # else:
+        stop("Unrecognized extension.")
+        }
+    }
+
+
+#' @rdname readalignment 
+read_fasta = function(file){
+    text = readLines(file)
+
+    from = grep(">", text)
+    to = c(from[2:length(from)] - 1, length(text))
+
+    sequences = parse_fasta_sequences(from, to, text)
+
+    return(sequences)
+    }
+
+
+#' @rdname readalignment
+read_nexus = function(file){
+    text = readLines(file)
+
+    # Find start and end of the data block:
+    begin_data = grep("^begin data;", text, ignore.case=TRUE)
+    ends = grep("^end;", text, ignore.case=TRUE)
+    end_data = ends[which.min(abs(ends - begin_data))]
+
+    # Find the start and end of the data matrix
+    begin_matrix = grep("^matrix", ignore.case=TRUE, text[begin_data:end_data]) + begin_data - 1
+    end_matrix = grep("^;", text[begin_matrix:end_data])[1] + begin_matrix - 1
+
+    header = parse_nexus_header(text[(begin_data+1):(begin_matrix-1)])
+    sequences = parse_nexus_sequences(text[(begin_matrix+1):(end_matrix-1)])
+
+    return(sequences)
+    }
+
+
+#' Internal helper functions for reading alignment
+#'
+#' These functions are used internally when alignemnt is being read and processed. They tend to
+#' extract and proces sequences from particular part of text.
+#'
+#' @param text vector of lines with sequences in particular format
+#' @param from in a fasta file, this is a vector of begining of sequences
+#' @param to in a fasta file, this is a vector of ends of sequences
+#' @name readalignmenthelper
+NULL
+
+
+#' @rdname readalignmenthelper
+parse_nexus_header = function(text){
+    text = sub(";$", "", text)
+    text = strsplit(text, split=" ", fixed=TRUE)
+    text = unlist(text)
+    text = grep("=", text, value=TRUE)
+    text = strsplit(text, split="=", fixed=TRUE)
+    header = lapply(text, getElement, 2)
+    names(header) = lapply(text, getElement, 1)
+    header
+    }
+
+
+#' @rdname readalignmenthelper
+parse_nexus_sequences = function(text){
+    text = strsplit(text, split="[[:blank:]]+")
+    sequences = lapply(text, getElement, 2)
+    names(sequences) = lapply(text, getElement, 1)
+    sequences
+    }
+
+
+#' @rdname readalignmenthelper
+parse_fasta_sequences = function(from, to, text){
+    sequences = mapply(
+        function(x,y,text){
+            paste0(text[(x+1):y], collapse="")
+            },
+        from, to, MoreArgs=list(text=text), SIMPLIFY=FALSE)
+    names(sequences) = sub("^>", "", text[from])
+    sequences
+    }
+
+
+#' Convert sequences to BEAST xml data block.
+#'
+#' This functions converts the parsed sequences into an XML data block for BEAST.
+#'
+#' @param sequences parsed from file
+#' @param data other variables, such as specification of dataType
+#' @return BEAST xml data block
+sequences2xml = function(sequences, data=list()){
+    # if parameters are not set, try to guess them
+    if(is.null(data$datatype)) data$datatype = guess_datatype(sequences)
+    if(is.null(data$id)) data$id = "Alignment"
+
+    if(data$datatype == "standard"){
+        data$standard = TRUE
+        data$number_of_states = guess_number_of_states(sequences)
+        }
+
+    template_standard = paste0(
+        "    <userDataType",
+        " id=\"StandardDataType\"",
+        " spec=\"beast.evolution.datatype.StandardData\"",
+        " ambiguities=\"\"",
+        " nrOfStates=\"{{number_of_states}}\"\n"
+        )
+
+    template = paste0(
+        "<data id=\"{{id}}\" dataType=\"{{datatype}}\">\n",
+        "    {{#sequences}}\n",
+        "    <sequence taxon=\"{{name}}\">{{sequence}}</sequence>\n",
+        "    {{/sequences}}\n",
+        "{{#standard}}",
+        template_standard,
+        "{{/standard}}",       
+        "</data>\n")
+    sequences_tuples = mapply(
+        function(x,y){list(name=x, sequence=y)},
+        names(sequences), sequences,
+        SIMPLIFY=FALSE, USE.NAMES=FALSE
+        )
+    data$sequences = sequences_tuples
+
+    whisker::whisker.render(template, data)
+    }
+
+
+#' Guess datatype from sequences
+#'
+#' Guess data type from sequences. Currently two data types are supported:
+#' \itemize{
+#'      \item nucleotide
+#'      \item standard
+#'  }
+#' @param sequences
+#' @return datatype
+guess_datatype = function(sequences){
+    chars = lapply(head(sequences), substring, 1, 5)
+    chars = paste0(unlist(chars), collapse="")
+    chars = unique(strsplit(chars, "")[[1]])
+    if(all(chars %in% c("A", "C", "T", "G", "-", "N", "?")))
+        return("nucleotide")
+    if(all(chars %in% c(1:9, "-", "N", "?")))
+        return("standard")
+    stop("Unrecognized data type")
+    }
+
+
+#' Guess the number of states
+#'
+#' Guess the number of states in sequences
+#' @param sequences
+#' @return number of states
+guess_number_of_states = function(sequences){
+    chars = lapply(sequences, function(x) unique(strsplit(x, "")[[1]]))
+    chars = unique(unlist(chars))
+    chars = chars[!chars %in% c("-", "N", "?")]
+    length(chars)
+    }
---
+++ tests/testthat/test_sequences.r
@@ -0,0 +1,95 @@
+context("Processing alignment files")
+
+# define variables that are used across tests:
+sequences = list(
+    seq1 = "ACTGACTG",
+    seq2 = "CTGACTGA",
+    seq3 = "TGACTGAC"
+    )
+
+
+fasta_file = "test_files/test.fasta"
+nexus_file = "test_files/test.nex"
+
+sequences_xml = paste0(
+    "<data id=\"Alignment\" dataType=\"nucleotide\">\n",
+    "    <sequence taxon=\"seq1\">ACTGACTG</sequence>\n",
+    "    <sequence taxon=\"seq2\">CTGACTGA</sequence>\n",
+    "    <sequence taxon=\"seq3\">TGACTGAC</sequence>\n",
+    "</data>\n"
+    )
+
+
+sequences_xml_random = paste0(
+    "<data id=\"Alignment\" dataType=\"standard\">\n",
+    "    <sequence taxon=\"seq_1\">1</sequence>\n",
+    "    <sequence taxon=\"seq_2\">1</sequence>\n",
+    "    <sequence taxon=\"seq_3\">1</sequence>\n",
+    "    <userDataType",
+    " id=\"StandardDataType\"",
+    " spec=\"beast.evolution.datatype.StandardData\"",
+    " ambiguities=\"\"",
+    " nrOfStates=\"1\"\n",
+    "</data>\n"
+    )
+    
+
+# helper functions
+tempcopy = function(file){
+    temp = tempfile()
+    file.copy(file, temp, overwrite=TRUE)
+    return(temp) 
+    }
+
+
+random_sequences = function(states=1:9, nseq=10, length=10){
+    seqs = replicate(
+        nseq,
+        paste0(sample(as.character(states), size=length, replace=TRUE), collapse=""),
+        simplify=FALSE
+        )
+    names(seqs) = paste0("seq_", 1:nseq)
+    seqs
+    }
+
+
+# Test sequence reading and processing ----------------------------------------------------------- #
+test_that("Can read a sample fasta file", {
+    expect_equal(read_fasta(fasta_file), sequences)
+    })
+
+
+test_that("Can read a sample nexus file", {
+    expect_equal(read_nexus(nexus_file), sequences)
+    })
+
+
+test_that("Uses a correct function to read alignment when FORMAT is specified", {
+    expect_equal(read_alignment(tempcopy(fasta_file), format="fasta"), sequences)
+    expect_equal(read_alignment(tempcopy(nexus_file), format="nexus"), sequences)
+    })
+
+
+test_that("Uses a correct function to read alignment by guessing format from extension", {
+    expect_equal(read_alignment(fasta_file), sequences)
+    expect_equal(read_alignment(nexus_file), sequences)
+    })
+
+
+test_that("Guessing datatype and values from data", {
+    expect_identical( random_sequences(1,1,1), list("seq_1"="1"))
+    expect_identical( random_sequences(2,2,10), list("seq_1"=strrep(2, 10), "seq_2"=strrep(2, 10)) )
+    expect_equal( guess_number_of_states(random_sequences(1)), 1)
+    expect_equal( guess_number_of_states(random_sequences(1:2)), 2)
+    expect_equal( guess_number_of_states(random_sequences(1:4)), 4)
+    expect_equal( guess_number_of_states(sequences), 4)
+
+    expect_equal( guess_datatype(random_sequences()), "standard")
+    expect_equal( guess_datatype(sequences), "nucleotide")
+    })
+
+
+test_that("Can convert sequences to xml", {
+    expect_identical(sequences2xml(sequences), sequences_xml)
+    expect_identical(sequences2xml(random_sequences(1, 3, 1)), sequences_xml_random)
+    })
