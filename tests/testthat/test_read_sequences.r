ac7017928fe53e8e2049de7f837551b14bf14a70
Jiří Moravec <jiri.c.moravec@gmail.com>
2021-04-19 14:52:43 +1200

    Added additional tests

--- tests/testthat/test_read_sequences.r
+++ tests/testthat/test_read_sequences.r
@@ -1,27 +1,35 @@
 context("Read sequence alignment files")
 
 
-fasta_file = "test_files/test.fasta"
-nexus_file = "test_files/test.nex"
+test_fasta = "test_files/test.fasta"
+test_nexus = "test_files/test.nex"
+primates_fasta = "test_files/primates.fasta"
+primates_nexus = "test_files/primates.nex"
 
-
-test_that("Can read a sample fasta file", {
-    expect_identical(read_fasta(fasta_file), sequences)
-    })
-
-
-test_that("Can read a sample nexus file", {
-    expect_identical(read_nexus(nexus_file), sequences)
+test_that("Can read test sequence files", {
+    expect_identical(read_fasta(test_fasta), sequences)
+    expect_identical(read_nexus(test_nexus), sequences)
     })
 
 
 test_that("Uses a correct function to read alignment when FORMAT is specified", {
-    expect_identical(read_sequences(tempcopy(fasta_file), format="fasta"), sequences)
-    expect_identical(read_sequences(tempcopy(nexus_file), format="nexus"), sequences)
+    expect_identical(read_sequences(tempcopy(test_fasta), format="fasta"), sequences)
+    expect_identical(read_sequences(tempcopy(test_nexus), format="nexus"), sequences)
     })
  
  
 test_that("Uses a correct function to read alignment by guessing format from extension", {
-    expect_identical(read_sequences(fasta_file), sequences)
-    expect_identical(read_sequences(nexus_file), sequences)
+    expect_identical(read_sequences(test_fasta), sequences)
+    expect_identical(read_sequences(test_nexus), sequences)
     })
+
+
+test_that("Correctly reads primate file", {
+    seq_fas = read_sequences(primates_fasta)
+    seq_nex = read_sequences(primates_nexus)
+    
+    expect_identical(seq_fas, seq_nex)
+
+    expect_true(all( nchar(seq_fas) == 898))
+    expect_true(all( nchar(seq_nex) == 898)) 
+    }) 
